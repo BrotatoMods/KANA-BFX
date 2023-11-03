121d1cbd89c21a195a8cf05433401d31c8187b6e
Kai <kai@kana.jetzt>
2023-11-03 23:07:19 +0100

    fix: :bug: improved player porting

--- root/mods-unpacked/KANA-BFX/extensions/main.gd
+++ root/mods-unpacked/KANA-BFX/extensions/main.gd
@@ -120,36 +120,31 @@ func _KANA_on_timespan_timer_timeout(key: String, value: int, seconds: int) -> v
 	ModLoaderLog.debug("Removed effect, new value of %s is -> %s" % [key, str(RunData.effects[key])], KANA_BFX_LOG_NAME_MAIN)
 
 
-func KANA_get_opposite_collision_tile_position(tile_position: Vector2, direction: Vector2) -> Vector2:
+func KANA_get_opposite_collision_tile_position(position: Vector2, direction: Vector2) -> Vector2:
 	var current_zone = ZoneService.get_zone_data(RunData.current_zone).duplicate()
-	var opposite_tile_position: Vector2
-	var spawn_offset = 2
+	var opposite_position: Vector2
+	var spawn_offset = 1 * 64
 
 	if direction == Vector2(1, 0):
-		opposite_tile_position = Vector2(current_zone.width, tile_position.y)
+		opposite_position = Vector2((current_zone.width * 64) - spawn_offset, position.y)
 	elif direction == Vector2(-1, 0):
-		opposite_tile_position = Vector2(spawn_offset, tile_position.y)
+		opposite_position = Vector2(spawn_offset, position.y)
 	elif direction == Vector2(0, 1):
-		opposite_tile_position = Vector2(tile_position.x, current_zone.height)
+		opposite_position = Vector2(position.x, (current_zone.height * 64) - spawn_offset)
 	elif direction == Vector2(0, -1):
-		opposite_tile_position = Vector2(tile_position.x, spawn_offset)
+		opposite_position = Vector2(position.x, spawn_offset)
 
-	var local_position: Vector2 = _tile_map_limits.map_to_world(opposite_tile_position)
-	var global_position: Vector2 = _tile_map_limits.to_global(local_position)
-
-	return global_position
+	return opposite_position
 
 
 func _KANA_on_player_border_collided(collision: KinematicCollision2D) -> void:
-	if RunData.effects["kana_bfx_port_to_opposite_side"] == 0:
-		return
+	if RunData.effects["kana_bfx_port_to_opposite_side"]:
+		var tile_position_player: Vector2 = collision.collider.world_to_map(_player.position)
+		var tile_position_collider: Vector2 = tile_position_player - collision.normal
 
-	var tile_position_player: Vector2 = collision.collider.world_to_map(_player.position)
-	var tile_position_collider: Vector2 = tile_position_player - collision.normal
+		var opposite_position := KANA_get_opposite_collision_tile_position(collision.position, collision.normal)
 
-	var opposite_position := KANA_get_opposite_collision_tile_position(tile_position_collider, collision.normal)
-
-	_player.global_position = opposite_position
+		_player.global_position = opposite_position
 
 
 func KANA_create_temp_effect_timer(key: String, value: int, seconds: int) -> void:
